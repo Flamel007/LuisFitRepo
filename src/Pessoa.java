0e9dece6393af1ba2df05b5593bd7fdeeb85679f
Flamel007 <flamelblackdeath007@gmail.com>
2025-10-17 09:28:36 -0300

    adicionado getters e setters

---
+++ src/Pessoa.java
@@ -0,0 +1,36 @@
+
+public class Pessoa {
+	int id;
+	String nome;
+	int idade;
+	
+	public Pessoa(int idPar, String nomePar, int idadePar){
+		this.id = idPar;
+		this.nome = nomePar;
+		this.idade = idadePar; }
+	
+	public int PegarId(){
+		return id;
+	}
+	
+	public String PegarNome(){
+		return nome;
+	}
+	
+	public int PegarIdade(){
+		return idade;
+	}
+	
+	public void AlterarId(int idPar){
+		this.id = idPar;
+	}
+	
+	public void AlterarNome(String nomePar){
+		this.nome = nomePar;
+	}
+	
+	public void AlterarIdade(int idadePar){
+		this.idade = idadePar;
+	}
+
+}
