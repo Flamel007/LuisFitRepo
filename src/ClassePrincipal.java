875105ac513d7bb894b579da08546f37c653b0c5
Flamel007 <flamelblackdeath007@gmail.com>
2025-10-31 10:35:09 -0300

    Adicionado Atletas Backup

--- src/ClassePrincipal.java
+++ src/ClassePrincipal.java
@@ -2,8 +2,13 @@
 public class ClassePrincipal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//Instanciar objeto atleta = criar objeto
+		Atleta obat_1 = new Atleta(1, "Mikael", 17, "Futebol");
+		Atleta obat_2 = new Atleta(2, "Leticia", 17, "Hipismo");
+		
+		obat_1.mostrarInformacoes();
+		obat_2.mostrarInformacoes();
 	}
 
 }
